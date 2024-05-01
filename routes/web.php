6c08ffed5e35b698f42cabd5ccedd5596c6c2e56
rudmarv <rudmarv@gmail.com>
2024-05-01 10:53:31 +0800

    local commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [WelcomeController::class, 'index']);
+Route::resource('images', 'WelcomeController', ['only' => ['store', 'destroy']]);
