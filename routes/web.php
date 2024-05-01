626e93a23a689e9d3de2c01f9fd1093330de3aa7
rudmarv <rudmarv@gmail.com>
2024-05-01 15:07:39 +0800

    upload v6 commit

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ Route::get('/', function () {
     return view('s3upload');
 });
 
+
 // Route::post(uri: '/s3upload', action: [AwsUploadController::class, 'store'])->name("s3upload");
 Route::post('s3upload', [S3uploadController::class, 'store'])->name("s3upload");
+// Route::get('s3upload', function () {return view('s3upload');})->name("s3upload");
 // Route::get('/', [S3uploadController::class, 'index']);
 // Route::resource('s3upload', 'S3uploadController', ['only' => ['store']]);
