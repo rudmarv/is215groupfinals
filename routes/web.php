3ed03f83d90654a317ff9cc599a38e3be180b2f1
rudmarv <rudmarv@gmail.com>
2024-05-02 11:45:45 +0800

    upload v16 commit

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\S3uploadController;
-Route::get('/', function () {
-    return view('s3upload');
-});
-
-
-// Route::post(uri: '/s3upload', action: [AwsUploadController::class, 'store'])->name("s3upload");
+// Route::get('/', function () {
+//     return view('s3upload');
+// });
+Route::get('/', [S3uploadController::class, 'index']);
 Route::post('s3upload', [S3uploadController::class, 'store'])->name("s3upload");
-// Route::get('s3upload', function () {return view('s3upload');})->name("s3upload");
-// Route::get('/', [S3uploadController::class, 'index']);
-// Route::resource('s3upload', 'S3uploadController', ['only' => ['store']]);
