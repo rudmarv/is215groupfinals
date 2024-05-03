c3a4cca6e0659fa354a70961df47f58a9d49146f
rudmarv <rudmarv@gmail.com>
2024-05-04 06:57:51 +0800

    articles from JSON commitv15

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -5,11 +5,11 @@
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="flex w-full mx-auto sm:px-6 lg:px-8 bg-white shadow-sm sm:rounded-lg">
+        <div class="flex max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white shadow-sm sm:rounded-lg">
             <h1>List of Articles</h1>
-            <div class="flex">
+            
                 @foreach ($files as $file)
-                <div class="w-1/3"><img class="h-auto max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description"></div>
+                <div class=" w-1/3 overflow-hidden "><img class="h-auto max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description"></div>
                 @php
                     $url = 'https://is215finals.s3.amazonaws.com/articles/'.$file.'-article.json';
                     $jsonData = file_get_contents($url);
@@ -17,7 +17,7 @@
                 @endphp
 
                 @if ($data !== null)
-                <div class="w-2/3">
+                <div class=" w-2/3 overflow-hidden ">
                     <h2 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{{ $data['title'] }}</h1>
                     <p class="text-gray-500 ">{{ $data['article'] }}</p>
                 </div>
@@ -26,7 +26,7 @@
                 @endif
 
                 @endforeach
-                </div>
+                
         </div>
     </div>
 </x-app-layout>
