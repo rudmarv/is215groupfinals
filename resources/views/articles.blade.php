9981f7a45441f3d545019bf513b4f0df7096027c
rudmarv <rudmarv@gmail.com>
2024-05-04 07:41:50 +0800

    articles from JSON commitv24

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -7,9 +7,9 @@
     <div class="py-12">
         <div class="flex flex-wrap max-w-7xl mx-auto sm:px-6 lg:px-8">
             @foreach ($files as $file)
-            <div class="bg-white shadow-sm sm:rounded-lg my-6">
-                <div class="w-4/12 overflow-hidden">
-                    <img class="w-full max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description">
+            <div class="bg-white shadow-sm sm:rounded-lg my-6 p-6">
+                <div class="w-full overflow-hidden">
+                    <img class="w-auto max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description">
                 </div>
                 @php
                     $url = 'https://is215finals.s3.amazonaws.com/articles/'.$file.'-article.json';
@@ -18,7 +18,7 @@
                 @endphp
 
                 @if ($data !== null)
-                    <div class="w-8/12 overflow-hidden">
+                    <div class="w-full overflow-hidden">
                         <h2 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{{ $data['title'] }}</h2>
                     </div>
                     <div class="w-full overflow-hidden">
