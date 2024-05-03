5ec708ad5854a4263dccb495fb580bed32249cac
rudmarv <rudmarv@gmail.com>
2024-05-04 07:24:19 +0800

    articles from JSON commitv20

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -5,9 +5,11 @@
         </h2>
     </x-slot>
     <div class="py-12">
-        <div class="flex flex-wrap max-w-7xl mx-auto sm:px-6 lg:px-8 bg-white shadow-sm sm:rounded-lg">
-                @foreach ($files as $file)
-                <div class=" w-4/12 overflow-hidden "><img class="w-full max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description"></div>
+        <div class="flex flex-wrap max-w-7xl mx-auto sm:px-6 lg:px-8">
+            @foreach ($files as $file)
+                <div class="w-4/12 overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <img class="w-full max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description">
+                </div>
                 @php
                     $url = 'https://is215finals.s3.amazonaws.com/articles/'.$file.'-article.json';
                     $jsonData = file_get_contents($url);
@@ -15,18 +17,16 @@
                 @endphp
 
                 @if ($data !== null)
-                <div class=" w-8/12 overflow-hidden ">
-                    <h2 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{{ $data['title'] }}</h1>
-                </div>
-                <div class=" w-full overflow-hidden ">
-                    <p class="text-gray-500 ">{{ $data['article'] }}</p>
-                </div>
+                    <div class="w-8/12 overflow-hidden">
+                        <h2 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{{ $data['title'] }}</h2>
+                    </div>
+                    <div class="w-full overflow-hidden">
+                        <p class="text-gray-500">{{ $data['article'] }}</p>
+                    </div>
                 @else
                     <p></p>
                 @endif
-
-                @endforeach
-                
+            @endforeach
         </div>
     </div>
 </x-app-layout>
