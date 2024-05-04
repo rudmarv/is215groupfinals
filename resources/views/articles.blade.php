18ccfc4f61ed36d9f3ab674db419f68dd9473439
rudmarv <rudmarv@gmail.com>
2024-05-04 09:01:24 +0800

    articles from JSON commitv31

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -6,17 +6,10 @@
     </x-slot>
     <div class="py-12">
         <div class="flex flex-wrap max-w-7xl mx-auto sm:px-6 lg:px-8">
-        @foreach ($files as $file)
-        <p>{{$file}}</p>
-        @endforeach
-        
         @php
             $filesReversed = array_reverse($files);
         @endphp
-        @foreach ($filesReversed as $file)
-        <p>{{$file}}</p>
-        @endforeach
-            @foreach ($files as $file)
+            @foreach ($filesReversed as $file)
             <div class="bg-white shadow-sm sm:rounded-lg my-6 p-6">
                 <div class="w-full overflow-hidden">
                     <img class="w-auto max-w-lg rounded-lg mb-6" src="https://is215finals.s3.amazonaws.com/{{ $file }}" alt="image description">
