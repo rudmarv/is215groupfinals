884d40e4a4299556acde3410b5be8d56dda70519
rudmarv <rudmarv@gmail.com>
2024-05-04 08:39:35 +0800

    articles from JSON commitv26

--- app/Http/Controllers/S3bucketController.php
+++ app/Http/Controllers/S3bucketController.php
@@ -48,7 +48,7 @@ class S3bucketController extends Controller
             // List all objects in the bucket
             $objects = $s3Client->listObjectsV2([
                 'Bucket' => $bucketName,
-                'Sort' => 'Descending'
+                'Sort' => 'Ascending'
             ]);
 
             // Extract filenames from the object list excluding directories
